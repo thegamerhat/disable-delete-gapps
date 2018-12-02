4d6211b46d854482e7da118948df37a439d16e1a
Ali Rıza <aliriza.keskin@pardustopluluk.org>
2018-12-02 23:54:26 +0300

    add safe remove

--- app/src/main/java/org/droidtr/deletegapps/MainActivity.java
+++ app/src/main/java/org/droidtr/deletegapps/MainActivity.java
@@ -14,36 +14,55 @@ public class MainActivity extends Activity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        
+		if(Build.VERSION.SDK_INT>21){
+			setTheme(android.R.style.Theme_Material_Dialog);
+		}else if(Build.VERSION.SDK_INT>14){
+			setTheme(android.R.style.Theme_Holo_Dialog);
+		}else{
+			setTheme(android.R.style.Theme_Dialog);
+		}
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        LinearLayout.LayoutParams butparam=new LinearLayout.LayoutParams(-1,-1);
+        butparam.weight=1;
+		LinearLayout main = new LinearLayout(this);
 		LinearLayout ll = new LinearLayout(this);
+		LinearLayout ll2 = new LinearLayout(this);
 		final TextView tt = new TextView(this);
+		tt.setText("Disable: disable gapps\nRemove:delete gapps with gmscore\nClear:delete gapps without gmscore\nReboot: reboot system");
+		main.setPadding(20,10,20,10);
 		Button delete = new Button(this);
 		delete.setText("Delete");
+		delete.setSingleLine();
+		delete.setLayoutParams(butparam);
+		Button safe = new Button(this);
+		safe.setText("Clear");
+		safe.setLayoutParams(butparam);
+		safe.setSingleLine();
 		Button disable = new Button(this);
 		disable.setText("Disable");
+		disable.setLayoutParams(butparam);
+		disable.setSingleLine();
 		Button reboot =new Button(this);
 		reboot.setText("Reboot");
-		ll.setLayoutParams(new LinearLayout.LayoutParams(-1,-1));
+		reboot.setLayoutParams(butparam);
+		reboot.setSingleLine();
+		main.setOrientation(LinearLayout.VERTICAL);
+		main.setLayoutParams(new LinearLayout.LayoutParams(-1,-1));
+		ll.setLayoutParams(new LinearLayout.LayoutParams(-1,-2));
+		ll2.setLayoutParams(new LinearLayout.LayoutParams(-1,-2));
 		ll.addView(delete);
-		ll.addView(disable);
-		ll.addView(reboot);
-		ll.addView(tt);
-		setContentView(ll);
+		ll.addView(safe);
+		ll2.addView(disable);
+		ll2.addView(reboot);
+		main.addView(ll);
+		main.addView(ll2);
+		main.addView(tt);
+		setContentView(main);
 		super.onCreate(savedInstanceState);
 		try{
 			final Process process = Runtime.getRuntime().exec("su");
 			final DataOutputStream dataOutputStream = new DataOutputStream(process.getOutputStream());
-			dataOutputStream.writeBytes("mount -o rw,remount /system\n");
-			dataOutputStream.flush();
-			dataOutputStream.writeBytes("sed -i \"s/ro.setupwizard.mode=ENABLED/ro.setupwizard.mode=DISABLED/\" /system/build.prop \n");
-			dataOutputStream.flush();
-			dataOutputStream.writeBytes("rm -rf /data/app/*{g,G}oogle*  &\n");
-			dataOutputStream.flush();
-			dataOutputStream.writeBytes("rm -rf /data/data/*{g,G}oogle* &\n");
-			dataOutputStream.flush();
-			dataOutputStream.writeBytes("rm -rf /data/app/*com.android.vending* &\n");
-			dataOutputStream.flush();
-			dataOutputStream.writeBytes("rm -rf /data/data/*com.android.vending* &\n");
+			dataOutputStream.writeBytes("mount -o rw,remount /system\n");	
 			dataOutputStream.flush();
 			delete.setOnClickListener(new OnClickListener(){
 
@@ -51,13 +70,77 @@ public class MainActivity extends Activity
 				public void onClick(View p1)
 				{
 					try{
-					dataOutputStream.writeBytes("pm list package -f | grep google | sed \"s/=.*//\" | sed \"s/.*:/rm -rf /\"> /data/list\n");
-					dataOutputStream.flush();
-					dataOutputStream.writeBytes("pm list package -f | grep com.android.vending | sed \"s/=.*//\" | sed \"s/.*:/rm -rf /\">> /data/list\n");
-					dataOutputStream.flush();
-					dataOutputStream.writeBytes("sh /data/list\n");
-					dataOutputStream.flush();
-					tt.setText("Gapps deleted succesfuly.");
+						dataOutputStream.writeBytes("sed -i \"s/ro.setupwizard.mode=ENABLED/ro.setupwizard.mode=DISABLED/\" /system/build.prop \n");
+						dataOutputStream.flush();
+						dataOutputStream.writeBytes("rm -rf /data/app/*{g,G}oogle*  &\n");
+						dataOutputStream.flush();
+						dataOutputStream.writeBytes("rm -rf /data/data/*{g,G}oogle* &\n");
+						dataOutputStream.flush();
+						dataOutputStream.writeBytes("rm -rf /data/app/*com.android.vending* &\n");
+						dataOutputStream.flush();
+						dataOutputStream.writeBytes("rm -rf /data/data/*com.android.vending* &\n");
+						dataOutputStream.flush();
+						//delete all gapps
+						dataOutputStream.writeBytes("pm list package -f | grep -v \"ext.services\" | grep google | sed \"s/=.*//\" | sed \"s/.*:/rm -rf /\"> /data/list\n");
+						dataOutputStream.flush();
+						dataOutputStream.writeBytes("pm list package -f | grep com.android.vending | sed \"s/=.*//\" | sed \"s/.*:/rm -rf /\">> /data/list\n");
+						dataOutputStream.flush();
+						dataOutputStream.writeBytes("sh /data/list\n");
+						dataOutputStream.flush();
+						tt.setText("Gapps deleted succesfuly.");
+					}catch(Exception e){
+						tt.setText("Fail: "+e.toString());
+					}
+				}
+			});
+			safe.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View p1)
+				{
+					try{
+						dataOutputStream.writeBytes("rm -rf /data/app/*{g,G}oogle*  &\n");
+						dataOutputStream.flush();
+						dataOutputStream.writeBytes("rm -rf /data/data/*{g,G}oogle* &\n");
+						dataOutputStream.flush();
+						dataOutputStream.writeBytes("rm -rf /data/app/*com.android.vending* &\n");
+						dataOutputStream.flush();
+						dataOutputStream.writeBytes("rm -rf /data/data/*com.android.vending* &\n");
+						dataOutputStream.flush();
+						//delete without gmscore
+						String code="pm list package -f | grep google";
+						String[] list ={
+								"com.google.android.gms",
+								"com.google.android.ext.services",
+								"com.google.android.messaging",
+								"com.google.android.apps.nexuslauncher",
+								"com.google.android.dialer",
+								"com.google.android.setupwizard",
+								"com.google.android.packageinstaller",
+								"com.google.android.ext.shared",
+								"com.google.android.onetimeinitializer",
+								"com.google.android.configupdater",
+								"com.google.android.webview",
+								"com.google.android.syncadapters.contacts",
+								"com.google.android.gsf",
+								"com.google.android.partnersetup",
+								"com.google.android.feedback",
+								"com.google.android.printservice.recommendation",
+								"com.google.android.syncadapters.calendar",
+								"com.google.android.gsf.login",
+								"com.google.android.backuptransport",
+								"com.google.android.contacts",
+
+						};
+						for(int i=0;i<list.length;i++){
+							code=code+" | grep -v \""+list[i]+"\"";
+						}
+						code=code+" | sed \"s/=.*//\" | sed \"s/.*:/rm -rf /\"> /data/list\n";
+						dataOutputStream.writeBytes(code);
+						dataOutputStream.flush();
+						dataOutputStream.writeBytes("sh /data/list\n");
+						dataOutputStream.flush();
+						tt.setText("Gapps cleared succesfuly.");
 					}catch(Exception e){
 						tt.setText("Fail: "+e.toString());
 					}
@@ -90,7 +173,6 @@ public class MainActivity extends Activity
 						try{
 							dataOutputStream.writeBytes("reboot\n");
 							dataOutputStream.flush();
-							tt.setText("Gapps disabled succesfuly.");
 
 						}catch(Exception e){
 							tt.setText("Fail: "+e.toString());
